65b0a2316ffe0c849cd037eb55828a7038ae2e02
Tim Silva <tim17silva@gmail.com>
2023-05-29 17:23:43 +0000

    Add a 'submit' event listener to the form element. #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
+const $form = document.querySelector('form');
+const $title = document.querySelector('#title');
 const $photoLink = document.querySelector('#photolink');
+const $notes = document.querySelector('#notes');
 const $photo = document.querySelector('img');
 
 $photoLink.addEventListener('input', function (event) {
   $photo.setAttribute('src', event.target.value);
+});
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const submission = {
+    title: $title.value,
+    photo: $photoLink.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId
+  };
+
+  data.nextEntryId++;
+  data.entries.push(submission);
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 });
