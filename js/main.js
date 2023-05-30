a004891910efbff04cd0c651ae722170f1025ce4
Tim Silva <tim17silva@gmail.com>
2023-05-30 16:59:11 +0000

    Define a function named renderEntry with a single parameter named entry. This parameter represents a single object for a single entry from the data.entries array. The function should generate and return a DOM tree for a single entry that matches the entries you created in the unordered list. #2

--- js/main.js
+++ js/main.js
@@ -23,3 +23,35 @@ $form.addEventListener('submit', function (event) {
   $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 });
+
+function renderEntry(entry) {
+  const $li = document.createElement('li');
+
+  const $row = document.createElement('div');
+  $row.setAttribute('class', 'row');
+  $li.append($row);
+
+  const $columnHalf = document.createElement('div');
+  $columnHalf.setAttribute('class', 'column-half');
+  $row.append($columnHalf);
+
+  const $image = document.createElement('img');
+  $image.setAttribute('src', entry.photo);
+  $image.setAttribute('alt', 'Journal Photo');
+  $columnHalf.append($image);
+
+  const $columnHalf2 = document.createElement('div');
+  $columnHalf2.setAttribute('class', 'column-half');
+  $row.append($columnHalf2);
+
+  const $h2 = document.createElement('h2');
+  $h2.classList.add('prozo', 'alignwithimg');
+  $h2.textContent = entry.title;
+
+  const $p = document.createElement('p');
+  $p.classList.add('opensans');
+  $p.textContent = entry.notes;
+  $columnHalf2.append($h2, $p);
+  return $li;
+}
+renderEntry();
